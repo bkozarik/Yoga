692c1b61629b5aa0474349bc8f7a2089852ccdab
 bkozarik <bkozarik@iMac-bkozarik.local>
2019-12-30 01:52:01 +0200

    Add Timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,43 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Timer
+
+    let deadline = '2020-01-01';
+
+    function getTimeRemaning(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000 * 60 * 60)));
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = document.querySelector('.hours'),
+            minutes = document.querySelector('.minutes'),
+            seconds = document.querySelector('.seconds'),
+            timeInteval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaning(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInteval);
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
 });
